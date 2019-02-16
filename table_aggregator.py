bdb0490d3228e9361d5ebbddbfb6fdc2127ce149
Travis Howe <travis.howe@springventuregroup.com>
2019-02-16 14:27:17 -0600

    table aggregator working

--- table_aggregator.py
+++ table_aggregator.py
@@ -144,7 +144,11 @@ def the_big_one(book, df_number, df_origin, df_performance):
         df_temp['chapter'] = row['end_chapter']
         df_temp['date'] = row['date']
 
-        missed_probs = row['miss_lst'][row['end_chapter']]
+        if row['miss_lst']:
+            missed_probs = row['miss_lst'][row['end_chapter']]
+        else:
+            missed_probs = []
+
         df_temp['correct'] = df_temp.apply(lambda x: 0 if str(x['problem']) in missed_probs else 1, axis=1)
 
         df_grande_test = df_grande_test.append(df_temp)
@@ -152,108 +156,11 @@ def the_big_one(book, df_number, df_origin, df_performance):
     return df_grande_ass, df_grande_test
 
 
-def performance_over_time(df, book, kid):
-    def js_month(x):
-        x_lst = x.split('-')
-        x_lst[1] = str(int(x_lst[1]) - 1)
-        if len(x_lst[1]) == 1:
-            x_lst[1] = '0' + x_lst[1]
-        return '-'.join(x_lst)
-    df['date'] = df['date'].astype(str).apply(js_month)  # this zero indexes the month for js's benefit.
-
-    return df['correct'].\
-        groupby(df['date']).mean().\
-        reset_index(drop=False). \
-        assign(book=book, kid=kid, position=range(0, df['date'].unique().shape[0]))
-
-def origin_lst_expand(df, kid):
-    df = df.loc[df['problem'].astype(str).str.isdigit()].assign(kid=kid)
-
-    df['origin_lst'] = df['origin'].str.split(', ')
-    df['len_origin_lst'] = df['origin_lst'].map(len)
-    df1 = df.query('len_origin_lst == 1')
-    df2 = df.query('len_origin_lst == 2')
-    return df1. \
-        append(
-        df2.assign(origin=df2['origin_lst'].map(lambda x: x[0])).append(
-            df2.assign(origin=df2['origin_lst'].map(lambda x: x[1])))
-        ). \
-        reset_index(drop=True). \
-        drop(['origin_lst', 'len_origin_lst'], 1)
-
-
-def mixed_problems_correct(df):
-    def counter(x):
-        x['position'] = range(1, len(x) + 1)
-        return x
-    return df. \
-        sort_values(['chapter', 'problem', 'origin']). \
-        groupby([df['chapter'], df['origin']]).apply(counter)
-
-
-def _sorter(x):
-    x_i = x.loc[~x['origin'].str.strip().str.isdigit()]
-    x_d = x.loc[x['origin'].str.strip().str.isdigit()]
-
-    x_d['origin'] = x_d['origin'].astype(int)
-    x_d.sort_values('origin', inplace=True)
-    x_d['origin'] = x_d['origin'].astype(str)
-
-    return x_d.append(x_i)
-
-def _chapter_sort(df):
-    if np.all(df['origin'].str.strip().str.isdigit()):
-        df['origin'] = df['origin'].astype(int)
-        df.sort_values(['mean', 'origin'], inplace=True)
-    else:
-        df.sort_values('mean', inplace=True)
-        df = df.groupby(df['mean']).apply(_sorter)
-    return df
-
-def performance_by_chapter(df):
-    df['origin'] = df['origin'].str.strip()
-    return pd.concat(
-        [
-            df['correct'].groupby(df['origin']).agg(['mean', 'count']),
-            df[['book', 'kid']].groupby(df['origin']).agg(lambda x: x.value_counts().index[0])
-        ],
-        axis=1
-        ). \
-        reset_index(drop=False). \
-        pipe(_chapter_sort)
-
-
-def performance_on_tests(df, kid):
-    if df.empty:
-        return df
-
-    df['kid'] = kid
-
-    df_miss = df.query('correct == 0')
-    df_miss_lst = df_miss.groupby(df_miss['chapter']).apply(lambda x: x['problem'].tolist())
-
-    df_test = pd.concat(
-        [
-            df['correct'].groupby(df['chapter']).mean(),
-            df_miss_lst,
-            df[['book', 'kid']].groupby(df['chapter']).agg(lambda x: x.value_counts().index[0])
-
-        ],
-        axis=1
-    ). \
-        reset_index(drop=False). \
-        rename(columns={'chapter': 'test', 0: 'miss_lst', 'correct': 'perc_correct'})
-    df_test['ind'] = df_test.index
-    df_test['chapters'] = df_test['test'].str.split().apply(lambda x: '{0}-{1}'.format(int(x[1]) * 4 - 3, int(x[1]) * 4))
-    var_lst = ['ind', 'book', 'kid', 'miss_lst', 'test', 'perc_correct', 'chapters']
-    return df_test[var_lst]
-
 def query_performance(name):
     df_assignment = pd.DataFrame()
     df_test = pd.DataFrame()
 
     for book in ['Math_5_4', 'Math_6_5', 'Math_7_6', 'Math_8_7', 'Algebra_1_2', 'Algebra_1', 'Algebra_2']:
-        print(book)
         perf_temp = pd.DataFrame(list(db_performance[book].find({'kid': name})))
         numb_temp = pd.DataFrame(list(db_number[book].find()))
         orig_temp = pd.DataFrame(list(db_origin[book].find()))
@@ -273,19 +180,14 @@ def db_writer(user, df):
     ret = db_math_aggregate[user].insert_many(df)
     print(ret.inserted_ids)
 
-
-
-
 def main():
-    # print(user_list())
-    # qp = query_performance('Calvin')
-    # print(qp)
+    for user in user_list():
+        qp = query_performance(user).reset_index(drop=True)
+        qp['name'] = user
+        qp['date'] = qp['date'].dt.date.astype(str)
 
-
-    db_writer(
-        'Calvin',
-        [{'name': 'Calvin', 'time': str(datetime.datetime.today())}]
-    )
+        # [{'name': 'Calvin', 'time': str(datetime.datetime.today())}]
+        db_writer(user, qp.to_dict(orient='records'))
 
 if __name__ == '__main__':
     main()
