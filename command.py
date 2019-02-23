851039066cd1ff6d34a8c6328b279abad6e4a7c4
Travis Howe <travis.howe@springventuregroup.com>
2019-02-23 15:53:53 -0600

    gunicorn server

--- command.py
+++ command.py
@@ -593,7 +593,5 @@ def database_viewer():
 
 
 
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8001, debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=8001, debug=True)
