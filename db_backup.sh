9efb22db7f46ec45d1cb54623584b4b956bb2663
Travis Howe <travis.howe@springventuregroup.com>
2019-02-23 18:38:43 -0600

    backup update

--- db_backup.sh
+++ db_backup.sh
@@ -1,9 +1,3 @@
-#!/bin/sh
-
-cd /home/pi/PythonProjects/howeschool_app
-
-git pull origin master
-
 mongodump -d math_book_info -o dump
 mongodump -d math_exercise_origins -o dump
 mongodump -d math_performance -o dump
@@ -12,8 +6,3 @@ mongodump -d scripture_commentary -o dump
 mongodump -d forms -o dump
 mongodump -d banking -o dump
 mongodump -d users -o dump
-
-git add dump/.
-git commit -m "standard mongo dump"
-git push origin master
-
