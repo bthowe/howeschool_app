ef6552f62689e0b301f6776a175868803d3db0d4
Travis Howe <travis.howe@springventuregroup.com>
2019-02-23 18:43:31 -0600

    backup update2

--- db_backup.sh
+++ db_backup.sh
@@ -1,8 +1,8 @@
-mongodump -d math_book_info -o dump
-mongodump -d math_exercise_origins -o dump
-mongodump -d math_performance -o dump
-mongodump -d vocab -o dump
-mongodump -d scripture_commentary -o dump
-mongodump -d forms -o dump
-mongodump -d banking -o dump
-mongodump -d users -o dump
+mongodump -d math_book_info -o /media/pi/HOWESCHOOL/database_files
+mongodump -d math_exercise_origins -o /media/pi/HOWESCHOOL/database_files
+mongodump -d math_performance -o /media/pi/HOWESCHOOL/database_files
+mongodump -d vocab -o /media/pi/HOWESCHOOL/database_files
+mongodump -d scripture_commentary -o /media/pi/HOWESCHOOL/database_files
+mongodump -d forms -o /media/pi/HOWESCHOOL/database_files
+mongodump -d banking -o /media/pi/HOWESCHOOL/database_files
+mongodump -d users -o /media/pi/HOWESCHOOL/database_files
